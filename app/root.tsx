d7df2f78010d364396a37f2fa1829e5dbf25136d
sergnio <sergnioa@gmail.com>
2022-02-27 15:25:53 -0600

    the things

--- app/root.tsx
+++ app/root.tsx
@@ -4,13 +4,39 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLVomw9EcEAbbqOCDtQ0jE_bOiPXg4_Yk",
+  authDomain: "yourrecipes-a1fe7.firebaseapp.com",
+  projectId: "yourrecipes-a1fe7",
+  storageBucket: "yourrecipes-a1fe7.appspot.com",
+  messagingSenderId: "138160383362",
+  appId: "1:138160383362:web:34e42e5c3ef84011d71b9b",
+  measurementId: "G-NBRXPS85MB",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+let analytics;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+  console.log({ analytics });
+}
 
 export default function App() {
   return (
