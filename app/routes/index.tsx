95db3273829f15dff594ead9b3edc30d81075c76
sergnio <sergnioa@gmail.com>
2022-02-27 15:53:52 -0600

    add prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,28 @@
+import prisma from "../../lib/prisma";
+import { LoaderFunction, useLoaderData } from "remix";
+import invariant from "tiny-invariant";
+
+export const loader: LoaderFunction = async ({ params: { slug } }) => {
+  // invariant(slug, "expected params.slug");
+  const feed = await prisma.recipe.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return feed;
+  // return getPost(slug);
+};
+
 export default function Index() {
+  const feed = useLoaderData<Recipe>();
+  console.log({ feed });
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome OSS Recipes</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <ul>{feed}</ul>
     </div>
   );
 }
