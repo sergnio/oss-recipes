7ac493c762a60b5e6ed918c227e3939c858b1065
sergnio <sergnioa@gmail.com>
2022-02-28 19:42:59 -0600

    brain login page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,23 @@
 import { Link, LoaderFunction, useLoaderData, useTransition } from "remix";
 import { PromiseReturnType } from "@prisma/client/scripts/default-index";
 import getRecipes from "~/data/getRecipes";
+import brainStyled from "~/styles/brain.css";
 
+export const links = () => {
+  return [{ rel: "stylesheet", href: brainStyled }];
+};
 export default () => (
-  <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-    <h1>Welcome OSS Recipes</h1>
+  <div className="container rootContainer" style={{ lineHeight: "1.4" }}>
+    {/*<h1>Welcome OSS Recipes</h1>*/}
+    <h1>Log in</h1>
+    {/*Eina, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial*/}
+    <div className="flex container">
+      <input className="rounded" placeholder="Email" />
+      <input className="rounded" placeholder="Password" />
+    </div>
+    <p>
+      Forgot password? <a className="resetBtn">Reset it</a>
+    </p>
+    <button className="rounded loginBtn">Log in</button>
   </div>
 );
